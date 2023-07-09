c54a63591df204a2661e02a668996f6f68d9e044
fire <fire@888.com>
2023-07-09 18:47:10 +0800

    nacos client.

---
+++ fox-nacos-client/src/main/java/top/latke/service/NacosClientService.java
@@ -0,0 +1,25 @@
+package top.latke.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+public class NacosClientService {
+
+    private final DiscoveryClient discoveryClient;
+
+    public NacosClientService(DiscoveryClient discoveryClient) {
+        this.discoveryClient = discoveryClient;
+    }
+
+    public List<ServiceInstance> getNacosClientInfo(String serviceId) {
+        log.info("request nacos client info [{]]",serviceId);
+        return discoveryClient.getInstances(serviceId);
+    }
+}
