8e48e14fb49fa885fff3a2144f69b2d27a36d3de
fire <fire@888.com>
2023-07-10 16:24:00 +0800

    fox-admin.

---
+++ fox-admin/src/main/java/top/latke/FoxAdminApplication.java
@@ -0,0 +1,18 @@
+package top.latke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 工程启动入口
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class NacosClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosClientApplication.class,args);
+    }
+
+}
