1af6aaa0ec434f8590e0d13ffd0f9159e11a023d
fire <fire@888.com>
2023-07-11 15:59:10 +0800

    fox-authority-center AuthorityController.

---
+++ fox-authority-center/src/main/java/top/latke/controller/AuthorityController.java
@@ -0,0 +1,55 @@
+package top.latke.controller;
+
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import top.latke.annotation.IgnoreResponseAdvice;
+import top.latke.service.IJWTService;
+import top.latke.vo.JwtToken;
+import top.latke.vo.UsernameAndPassword;
+
+/**
+ * 对外暴露的授权服务接口
+ */
+@Slf4j
+@RestController
+@RequestMapping("authority")
+public class AuthorityController {
+
+    private final IJWTService ijwtService;
+
+    public AuthorityController(IJWTService ijwtService) {
+        this.ijwtService = ijwtService;
+    }
+
+    /**
+     * 从授权中心获取 Token，且返回信息中没有统一响应的包装
+     * @param usernameAndPassword
+     * @return
+     * @throws Exception
+     */
+    @IgnoreResponseAdvice
+    @PostMapping("token")
+    public JwtToken token(@RequestBody UsernameAndPassword usernameAndPassword) throws Exception{
+        log.info("method-token request to get token with param: [{}]", JSON.toJSONString(usernameAndPassword));
+        return new JwtToken(ijwtService.generateToken(usernameAndPassword.getUsername(),usernameAndPassword.getPassword()));
+    }
+
+    /**
+     * 注册用户并从授权中心获取 Token，且返回信息中没有统一响应的包装
+     * @param usernameAndPassword
+     * @return
+     * @throws Exception
+     */
+    @IgnoreResponseAdvice
+    @PostMapping("register")
+    public JwtToken register(@RequestBody UsernameAndPassword usernameAndPassword) throws Exception{
+        log.info("method-register request to get token with param: [{}]", JSON.toJSONString(usernameAndPassword));
+        return new JwtToken(ijwtService.generateToken(usernameAndPassword.getUsername(),usernameAndPassword.getPassword()));
+    }
+
+}
