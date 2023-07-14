cde80b229b77259ad6a2d614d7daf87ba6128399
fire <fire@888.com>
2023-07-14 16:23:57 +0800

    Gateway Testcase.

--- fox-gateway/src/main/java/top/latke/filter/factory/HeaderTokenGatewayFilterFactory.java
+++ fox-gateway/src/main/java/top/latke/filter/factory/HeaderTokenGatewayFilterFactory.java
@@ -6,10 +6,20 @@ import org.springframework.stereotype.Component;
 import top.latke.filter.HeaderTokenGatewayFilter;
 
 /**
- * 使 HeaderTokenGatewayFilter 局部过滤器生效
+ * 使 HeaderTokenGatewayFilterFactory 局部过滤器生效,命名必须是 prefix + GatewayFilterFactory，否则会发生找不到的情况
+ * 	"filters": [{
+ * 			"name": "HeaderToken"
+ *                },
+ *        {
+ * 			"name": "StripPrefix",
+ * 			"args": {
+ * 				"parts": "1"
+ *            }
+ *        }
+ * 	]
  */
 @Component
-public class HeaderTokenGatewayFactory extends AbstractGatewayFilterFactory<Object> {
+public class HeaderTokenGatewayFilterFactory extends AbstractGatewayFilterFactory<Object> {
     @Override
     public GatewayFilter apply(Object config) {
         return new HeaderTokenGatewayFilter();
