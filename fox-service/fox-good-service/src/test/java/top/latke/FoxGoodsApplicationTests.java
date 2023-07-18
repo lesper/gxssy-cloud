8279c79120713a22acae2bbb36e40ad1d0fdaac0
fire <fire@888.com>
2023-07-18 15:14:21 +0800

    Goods Test case.

--- fox-service/fox-good-service/src/test/java/top/latke/FoxGoodsApplicationTests.java
+++ fox-service/fox-good-service/src/test/java/top/latke/FoxGoodsApplicationTests.java
@@ -5,13 +5,9 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-/**
- * 授权中心测试入口,验证授权中心环境可用性
- */
-
 @SpringBootTest
 @RunWith(SpringRunner.class)
-public class AuthorityCenterApplicationTests {
+public class FoxGoodsApplicationTests {
 
     @Test
     public void contextLoads() {
