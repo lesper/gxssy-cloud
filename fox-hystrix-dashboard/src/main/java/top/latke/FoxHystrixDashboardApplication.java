a5d9d0d5b1dbfc88228932faf2a1b63d95b8e8e6
fire <fire@888.com>
2023-07-25 00:39:53 +0800

    hystrix dashboard.

--- fox-hystrix-dashboard/src/main/java/top/latke/FoxHystrixDashboardApplication.java
+++ fox-hystrix-dashboard/src/main/java/top/latke/FoxHystrixDashboardApplication.java
@@ -3,10 +3,14 @@ package top.latke;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 /**
  * 工程启动入口
+ * Hystrix Dashboard: http://localhost:9999/fox-hystrix-dashboard/hystrix
+ * nacos client: http://127.0.0.1:8000/fox-nacos-client/actuator/hystrix.stream
  */
+@EnableHystrixDashboard
 @EnableDiscoveryClient
 @SpringBootApplication
 public class FoxHystrixDashboardApplication {
