4f84099d35854671087a0791710094771d4fc2d9
fire <fire@888.com>
2023-07-25 00:12:32 +0800

    hystrix-command + feign-fallback-factory 简化合并故障转移 Test case.

--- fox-nacos-client/src/main/java/top/latke/service/communication/AuthorityFeignClient.java
+++ fox-nacos-client/src/main/java/top/latke/service/communication/AuthorityFeignClient.java
@@ -4,14 +4,15 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import top.latke.service.communication.hystrix.AuthorityFeignClientFallback;
+import top.latke.service.communication.hystrix.AuthorityFeignClientFallbackFactory;
 import top.latke.vo.JwtToken;
 import top.latke.vo.UsernameAndPassword;
 
 /**
  * 与 Authority 通信的 Feign Client 接口定义
  */
-@FeignClient(contextId = "AuthorityFeignClient",value = "fox-authority-center",fallback = AuthorityFeignClientFallback.class)
+//@FeignClient(contextId = "AuthorityFeignClient",value = "fox-authority-center",fallback = AuthorityFeignClientFallback.class)
+@FeignClient(contextId = "AuthorityFeignClient",value = "fox-authority-center",fallbackFactory = AuthorityFeignClientFallbackFactory.class)
 public interface AuthorityFeignClient {
 
     /**
---
+++ fox-nacos-client/src/main/java/top/latke/service/communication/hystrix/AuthorityFeignClientFallbackFactory.java
@@ -0,0 +1,26 @@
+package top.latke.service.communication.hystrix;
+
+import feign.hystrix.FallbackFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import top.latke.service.communication.AuthorityFeignClient;
+import top.latke.vo.JwtToken;
+import top.latke.vo.UsernameAndPassword;
+
+/**
+ *  AuthorityFeignClient 后备 Fallback-Factory-OpenFeign 实现
+ */
+@Slf4j
+@Component
+public class AuthorityFeignClientFallbackFactory implements FallbackFactory<AuthorityFeignClient> {
+    @Override
+    public AuthorityFeignClient create(Throwable throwable) {
+        log.warn("authority feign client get token by feign request error(Hystrix FallbackFactory: [{}])",throwable.getMessage(),throwable);
+        return new AuthorityFeignClient() {
+            @Override
+            public JwtToken getTokenByFeign(UsernameAndPassword usernameAndPassword) {
+                return new JwtToken("admin-factory");
+            }
+        };
+    }
+}
