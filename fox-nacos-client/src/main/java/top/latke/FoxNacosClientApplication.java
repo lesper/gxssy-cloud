de52e304e63d23b81f20bd8e6bf1b63465af1f8e
fire <fire@888.com>
2023-07-28 21:00:18 +0800

    gateway #sentinel.

--- fox-nacos-client/src/main/java/top/latke/FoxNacosClientApplication.java
+++ fox-nacos-client/src/main/java/top/latke/FoxNacosClientApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
@@ -13,7 +14,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @ServletComponentScan
 @EnableCircuitBreaker
 @EnableFeignClients
-//@RefreshScope
+@RefreshScope
 @EnableDiscoveryClient
 @SpringBootApplication
 public class FoxNacosClientApplication {
--- fox-nacos-client/src/main/java/top/latke/controller/NacosClientController.java
+++ fox-nacos-client/src/main/java/top/latke/controller/NacosClientController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import top.latke.config.ProjectConfig;
 import top.latke.service.NacosClientService;
 
 import java.util.List;
@@ -17,13 +18,25 @@ public class NacosClientController {
 
     private final NacosClientService nacosClientService;
 
-    public NacosClientController(NacosClientService nacosClientService) {
+    private final ProjectConfig projectConfig;
+
+    public NacosClientController(NacosClientService nacosClientService, ProjectConfig projectConfig) {
         this.nacosClientService = nacosClientService;
+        this.projectConfig = projectConfig;
     }
 
     @GetMapping("service-instance")
     public List<ServiceInstance> logNacosClientInfo(@RequestParam(defaultValue = "fox-nacos-client") String serviceId){
         log.info("coming in log nacos client info: [{}]",serviceId);
         return nacosClientService.getNacosClientInfo(serviceId);
+    }
+
+
+    /**
+     * <h2>动态获取 Nacos 中的配置信息</h2>
+     * */
+    @GetMapping("/project-config")
+    public ProjectConfig getProjectConfig() {
+        return projectConfig;
     }
 }
