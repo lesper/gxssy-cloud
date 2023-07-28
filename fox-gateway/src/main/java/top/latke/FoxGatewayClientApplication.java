c4d28b799a651b39431a4c89995afb4983be1f9f
fire <fire@888.com>
2023-07-28 21:23:15 +0800

    gateway #sentinel.

--- fox-gateway/src/main/java/top/latke/FoxGatewayClientApplication.java
+++ fox-gateway/src/main/java/top/latke/FoxGatewayClientApplication.java
@@ -11,6 +11,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 public class FoxGatewayClientApplication {
     public static void main(String[] args) {
+        System.setProperty("csp.sentinel.app.type","1");
         SpringApplication.run(FoxGatewayClientApplication.class,args);
     }
 
